b66bce6e5295cba3c5dc2bd381d475a9127a0d27
d.petrov <d.petrov@eastwind.ru>
2026-08-13 15:33:52 +0300

    Initial project setup

---
+++ src/test/java/ru/petrov/ci_cd/CiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.petrov.ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
